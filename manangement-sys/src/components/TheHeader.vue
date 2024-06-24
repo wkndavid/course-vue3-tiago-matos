03e2c00b262f0fb18e0084ac32f1fa0c0a8103f3
wkndavid <workingnown@gmail.com>
2024-06-24 14:23:00 -0300

    init app with Header example

---
+++ manangement-sys/src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        HEADER
+    </header>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    header {
+        background: #45CB9C;
+        color: #35495E;
+        font-weight: bolder;
+        padding: 15px;
+}
+</style>
